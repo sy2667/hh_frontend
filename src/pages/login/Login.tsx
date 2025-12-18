d38d5633fffd33ea3af7614c65ae4c58bb097102
홍신영 <usthong@ust21.co.kr>
2025-12-18 15:43:34 +0900

    [MOD] console.log 확인

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -35,7 +35,8 @@ const Login = () => {
 
   useEffect(() => {
     const temp = async (e: MessageEvent) => {
-      console.log('오리진 이전')
+      console.log(`${APP_ORIGIN} 오리진 전`)
+      console.log(`${e.origin} 오리진 전`)
       if (e.origin !== APP_ORIGIN) return
       console.log(`${APP_ORIGIN} 오리진 이후`)
       console.log(`${e.origin} 오리진 이후`)
